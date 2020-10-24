1bc5f9d75ccc7d93c673443eaf840f9efc27e1d6
Vitaly Galperin <vitalyglll@gmail.com>
2020-10-24 15:18:18 +0300

    Task 1 is ready for verification

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -1,14 +1,20 @@
 from django.http import HttpResponse
 
 from django.views import View
+import random
 
 
 class ToDoView(View):
 
     def get(self, request, *args, **kwargs):
-        return HttpResponse('<ul>'
-                            '<li>Установить python</li>'
-                            '<li>Установить django</li>'
-                            '<li>Запустить сервер</li>'
-                            '<li>Порадоваться результату</li>'
-                            '</ul>')
+        return HttpResponse(create_response())
+
+
+def create_response():
+    list_items = ['<li>Пункт 1</li>', '<li>Пункт 2</li>', '<li>Пункт 3</li>', '<li>Пункт 4</li>', '<li>Пункт 5</li>']
+    random.shuffle(list_items)
+    response = '<ul>'
+    for item in list_items:
+        response += item
+    response += '<ul>'
+    return response
