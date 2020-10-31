fa4fcc228ea6d3b9897fc1f1f9b2a0f655b54379
Vitaly Galperin <vitalyglll@gmail.com>
2020-10-31 12:42:18 +0200

    Part 3 lesson 3.5; 3.6 ready

--- 03_RequestsHandlingInDjango/board/advertisements/urls.py
+++ 03_RequestsHandlingInDjango/board/advertisements/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.advertisement_list, name='advertisement_list'),
     path('contacts', views.contacts, name='contacts'),
-    path('about', views.about, name='about'),
+    path('about/', views.About.as_view()),
     path('categories', views.categories, name='categories'),
-    path('regions', views.regions, name='regions'),
+    path('regions/', views.Regions.as_view()),
 ]
