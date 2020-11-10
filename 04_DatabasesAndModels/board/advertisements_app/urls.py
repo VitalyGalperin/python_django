ee86d6321adf95ca281ae1f06fc6fd55c4ee6b9a
Vitaly Galperin <vitalyglll@gmail.com>
2020-11-10 19:31:13 +0200

    Part 4 is ready

--- 04_DatabasesAndModels/board/advertisements_app/urls.py
+++ 04_DatabasesAndModels/board/advertisements_app/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.main, name='Список объявлений'),
-    path('fill_db', views.fill_db, name='Добавить 100 записей в БД'),
-    path('random', views.random, name='Случайное объявление'),
+    path('', views.MainView.as_view(), name='Список объявлений'),
+    path('fill_db', views.FillDB.as_view(), name='Добавить 100 записей в БД'),
+    path('random', views.RandomView.as_view(), name='Случайное объявление'),
     path('advertisements', views.AdsListView.as_view(), name='Список объявлений'),
     path('advertisements/<int:pk>', views.AdsDetailView.as_view(), name='Список объявлений детали'),
 ]
--- 04_DatabasesAndModels/board/advertisements_app/views.py
+++ 04_DatabasesAndModels/board/advertisements_app/views.py
@@ -1,21 +1,24 @@
 from random import randint
 import datetime
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from django.shortcuts import render
 from .models import Advertisement
+from .forms import CountForm
 
 
-def main(request, *args, **kwargs):
-    advertisements = Advertisement.objects.all()[:3]
-    content = {'advertisements': advertisements}
-    return render(request, 'advertisements_app/main.html', content)
+class MainView(View):
+    def get(self, request):
+        advertisements = Advertisement.objects.all()[:3]
+        content = {'advertisements': advertisements}
+        return render(request, 'advertisements_app/main.html', content)
 
 
-def random(request, *args, **kwargs):
-    count = Advertisement.objects.count()
-    random_object = Advertisement.objects.all()[randint(0, count - 1)]
-    content = {'advertisements': random_object}
-    return render(request, 'advertisements_app/random.html', content)
+class RandomView(View):
+    def get(self, request):
+        count = Advertisement.objects.count()
+        random_object = Advertisement.objects.all()[randint(0, count - 1)]
+        content = {'advertisements': random_object}
+        return render(request, 'advertisements_app/random.html', content)
 
 
 class AdsListView(ListView):
@@ -29,15 +32,27 @@ class AdsDetailView(DetailView):
     model = Advertisement
 
 
-def fill_db(request, *args, **kwargs):
-    for i in range(0, 100):
-        new_advertisement = Advertisement(title='Объявление ' + str(i),
-                                          description='Текст объявления' + str(i),
-                                          price=i,
-                                          created_at=datetime.datetime.now(),
-                                          finish_at=datetime.datetime.now() + datetime.timedelta(weeks=5),
-                                          views_count=i
+class FillDB(View):
+    def get(self, request, *args, **kwargs):
+        count_form = CountForm()
+        content = {'message': '',
+                   'count_form': count_form}
+        return render(request, 'advertisements_app/fill_db.html', content)
 
-                                          )
-        new_advertisement.save()
-    return render(request, 'advertisements_app/fill_db.html')
+    def post(self, request, *args, **kwargs):
+        count_form = CountForm(request.POST)
+        content = {}
+        if count_form.is_valid():
+            count = count_form.cleaned_data['counter']
+            for i in range(0, count):
+                new_advertisement = Advertisement(title='Объявление ' + str(i),
+                                                  description='Текст объявления' + str(i),
+                                                  price=i,
+                                                  created_at=datetime.datetime.now(),
+                                                  finish_at=datetime.datetime.now() + datetime.timedelta(weeks=5),
+                                                  views_count=i
+                                                  )
+                new_advertisement.save()
+            content = {'message': 'Записи добавалены',
+                       'count_form': count_form}
+        return render(request, 'advertisements_app/fill_db.html', content)
