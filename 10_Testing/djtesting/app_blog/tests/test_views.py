54d172d3bdb61397c5656180a8eb8d5104d82a73
Vitaly Galperin <vitalyglll@gmail.com>
2021-04-11 19:22:34 +0300

    10 задание. Готово к проверке

--- 10_Testing/djtesting/app_blog/tests/test_views.py
+++ 10_Testing/djtesting/app_blog/tests/test_views.py
@@ -1,5 +1,5 @@
 import os
-
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
@@ -17,68 +17,75 @@ class BlogTest(TestCase):
         create_test_users_and_items()
         create_test_csv_file()
 
-    # def test_blog_exists_at_desired_location(self):
-    #     login = self.client.login(username='test_user_with_permissions', password='11111')
-    #     response = self.client.get('/blog')
-    #     self.assertEqual(str(response.context['user']), 'test_user_with_permissions')
-    #     self.assertTemplateUsed(response, 'app_blog/blog_list.html')
-    #
-    # def test_blog_detail_exists_at_desired_location(self):
-    #     login = self.client.login(username='test_user_without_permissions', password='22222')
-    #     response = self.client.get('/blog/1')
-    #     self.assertEqual(str(response.context['user']), 'test_user_without_permissions')
-    #     self.assertTemplateUsed(response, 'app_blog/blog_detail.html')
-    #
-    # def test_blog_number(self):
-    #     response = self.client.get(reverse('BlogListView'))
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTrue(len(response.context_data['blog']) == NUMBER_OF_ITEMS)
-    #
-    # def test_add_blog_without_permissions(self):
-    #     login = self.client.login(username='test_user_without_permissions', password='22222')
-    #     response = self.client.get(reverse('AddBlogView'))
-    #     self.assertEqual(response.status_code, 403)
-    #
-    # def test_add_blog_exists_at_desired_location(self):
-    #     login = self.client.login(username='test_user_with_permissions', password='11111')
-    #     response = self.client.get('/edit_blog')
-    #     self.assertEqual(str(response.context['user']), 'test_user_with_permissions')
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'app_blog/add_blog.html')
-    #
-    # def test_change_blog_without_permissions(self):
-    #     login = self.client.login(username='test_user_without_permissions', password='22222')
-    #     response = self.client.get('/edit_blog/1')
-    #     self.assertEqual(response.status_code, 403)
-    #
-    # def test_change_blog_exists_at_desired_location(self):
-    #     login = self.client.login(username='test_user_with_permissions', password='11111')
-    #     response = self.client.get('/edit_blog/1')
-    #     self.assertEqual(str(response.context['user']), 'test_user_with_permissions')
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'app_blog/edit_blog.html')
-    #
-    # def test_upload_blog_without_permissions(self):
-    #     login = self.client.login(username='test_user_without_permissions', password='22222')
-    #     response = self.client.get('/upload_blog')
-    #     self.assertEqual(response.status_code, 403)
-    #
-    # def test_upload_blog_exists_at_desired_location(self):
-    #     login = self.client.login(username='test_user_with_permissions', password='11111')
-    #     response = self.client.get('/upload_blog')
-    #     self.assertEqual(str(response.context['user']), 'test_user_with_permissions')
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'app_blog/Upload_blog.html')
+    def test_blog_exists_at_desired_location(self):
+        login = self.client.login(username='test_user_with_permissions', password='11111')
+        response = self.client.get('/blog')
+        self.assertEqual(str(response.context['user']), 'test_user_with_permissions')
+        self.assertTemplateUsed(response, 'app_blog/blog_list.html')
+
+    def test_blog_detail_exists_at_desired_location(self):
+        login = self.client.login(username='test_user_without_permissions', password='22222')
+        response = self.client.get('/blog/1')
+        self.assertEqual(str(response.context['user']), 'test_user_without_permissions')
+        self.assertTemplateUsed(response, 'app_blog/blog_detail.html')
+
+    def test_blog_number(self):
+        response = self.client.get(reverse('BlogListView'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(len(response.context_data['blog']) == NUMBER_OF_ITEMS)
+
+    def test_add_blog_without_permissions(self):
+        login = self.client.login(username='test_user_without_permissions', password='22222')
+        response = self.client.get(reverse('AddBlogView'))
+        self.assertEqual(response.status_code, 403)
+
+    def test_add_blog_exists_at_desired_location(self):
+        login = self.client.login(username='test_user_with_permissions', password='11111')
+        response = self.client.get('/edit_blog')
+        self.assertEqual(str(response.context['user']), 'test_user_with_permissions')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'app_blog/add_blog.html')
+
+    def test_change_blog_without_permissions(self):
+        login = self.client.login(username='test_user_without_permissions', password='22222')
+        response = self.client.get('/edit_blog/1')
+        self.assertEqual(response.status_code, 403)
+
+    def test_change_blog_exists_at_desired_location(self):
+        login = self.client.login(username='test_user_with_permissions', password='11111')
+        response = self.client.get('/edit_blog/1')
+        self.assertEqual(str(response.context['user']), 'test_user_with_permissions')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'app_blog/edit_blog.html')
+
+    def test_upload_blog_without_permissions(self):
+        login = self.client.login(username='test_user_without_permissions', password='22222')
+        response = self.client.get('/upload_blog')
+        self.assertEqual(response.status_code, 403)
+
+    def test_upload_blog_exists_at_desired_location(self):
+        login = self.client.login(username='test_user_with_permissions', password='11111')
+        response = self.client.get('/upload_blog')
+        self.assertEqual(str(response.context['user']), 'test_user_with_permissions')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'app_blog/Upload_blog.html')
 
     def test_upload_blog_verification_and_write_CSV(self):
         login = self.client.login(username='test_user_with_permissions', password='11111')
         test_file = os.path.join(BASE_DIR, 'app_blog', 'tests', 'test_file.csv')
-        with open(test_file, 'r', encoding='utf-8') as open_CSV_file:
-            CSV_file_in_memory = open_CSV_file.read()
-            CSV_file = reader(CSV_file_in_memory, delimiter=';', quotechar='"')
-            response = self.client.post(reverse('UploadCSVView'), {'CSV_file': CSV_file})
-        self.assertEqual(response['content-type'], 'application/vnd.ms-excel')
-        self.assertRedirects(response, reverse('/'))
+        with open(test_file, 'rb') as open_CSV_file:
+            open_csv_file = open_CSV_file.read()
+            csv_file = SimpleUploadedFile(content=open_csv_file, name='test_file.csv',
+                                          content_type='application/vnd.ms-excel')
+            response = self.client.post(reverse('UploadCSVView'), {'CSV_files': csv_file})
+        self.assertTrue(Blog.objects.get(title='title 1'))
+        try:
+            Blog.objects.get(title='title 2')
+        except:
+            self.assertFalse(False)
+        self.assertTrue(Blog.objects.get(title='title 3'))
+        self.assertRedirects(response, '/')
+        os.remove(test_file)
 
 
 def create_test_csv_file():
--- 10_Testing/djtesting/app_logic/tests/test_views.py
+++ 10_Testing/djtesting/app_logic/tests/test_views.py
@@ -1,42 +1,40 @@
-# from decimal import Decimal
-# from random import randint
-#
-# from django.test import TestCase
-# from django.urls import reverse
-#
-# from app_logic.models import Item
-#
-# NUMBER_OF_ITEMS = 10
-#
-#
-# class WelcomePageTest(TestCase):
-#     def test_welcome_page(self):
-#         url = reverse('welcome')
-#         response = self.client.get(url)
-#         self.assertEqual(response.status_code, 200)
-#         self.assertContains(response, 'Welcome')
-#
-#
-# class ItemsTest(TestCase):
-#     @classmethod
-#     def setUpTestData(cls):
-#         for item_index in range(NUMBER_OF_ITEMS):
-#             Item.objects.create(
-#                 name=f'name{item_index}',
-#                 code=f'code{item_index}',
-#                 price=Decimal(randint(1, 100))
-#             )
-#
-#     def test_items_exists_at_desired_location(self):
-#         response = self.client.get('/tests/item')
-#         self.assertEqual(response.status_code, 200)
-#         self.assertTemplateUsed(response, 'item_list.html')
-#
-#     def test_items_number(self):
-#         response = self.client.get(reverse('ItemListView'))
-#         self.assertEqual(response.status_code, 200)
-#         self.assertTrue(len(response.context_data['items']) == NUMBER_OF_ITEMS)
-#         # из лекции для View через функции
-#         # self.assertTrue(len(response.context['item_list']) == NUMBER_OF_ITEMS)
-#
-#
+from decimal import Decimal
+from random import randint
+
+from django.test import TestCase
+from django.urls import reverse
+
+from app_logic.models import Item
+
+NUMBER_OF_ITEMS = 10
+
+
+class WelcomePageTest(TestCase):
+    def test_welcome_page(self):
+        url = reverse('welcome')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Welcome')
+
+
+class ItemsTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        for item_index in range(NUMBER_OF_ITEMS):
+            Item.objects.create(
+                name=f'name{item_index}',
+                code=f'code{item_index}',
+                price=Decimal(randint(1, 100))
+            )
+
+    def test_items_exists_at_desired_location(self):
+        response = self.client.get('/tests/item')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'item_list.html')
+
+    def test_items_number(self):
+        response = self.client.get(reverse('ItemListView'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(len(response.context_data['items']) == NUMBER_OF_ITEMS)
+        # из лекции для View через функции
+        # self.assertTrue(len(response.context['item_list']) == NUMBER_OF_ITEMS)
